fec24acbf6e25dc474a6fa3a497f9b1b3b90b9fd
yibrah <yibrahg1@gmail.com>
2016-09-12 07:57:13 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <html>
 <body>
   <h1> This is my first project</h1>
-  <a href="http://htmlpreview.github.io/?https://github.com/yibrah/MIT-Projects/master/about.php">About</a>
+  <a href="http://htmlpreview.github.io/?https://github.com/yibrah/MIT-Projects/master/index.php">Home</a> |
+  <a href="http://htmlpreview.github.io/?https://github.com/yibrah/MIT-Projects/master/about.php">About</a> | 
+  <a href="http://htmlpreview.github.io/?https://github.com/yibrah/MIT-Projects/master/services.php">Services</a> | 
+  <a href="http://htmlpreview.github.io/?https://github.com/yibrah/MIT-Projects/master/contact.php">Contact</a>
 </body>
 </html>
