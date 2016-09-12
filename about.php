3e5ab239c9020b88a9c7b38f7389b896216a5f5f
hailay12 <hailaymk@gmail.com>
2016-09-12 18:16:49 +0300

    Update about.php

--- about.php
+++ about.php
@@ -1,6 +1,13 @@
 <html>
 <body>
 <h2> This is About</h2>
-<a href="http://htmlpreview.github.io/?https://github.com/yibrah/MIT-Projects/master/index.php">Home</a>
+<a href="http://htmlpreview.github.io/?https://github.com/yibrah/MIT-Projects/master/index.php">Home</a> <br/>
+<form>
+  First name:<br>
+  <input type="text" name="firstname">
+  <br>
+  Last name:<br>
+  <input type="text" name="lastname">
+</form>
 </body>
 </html>
