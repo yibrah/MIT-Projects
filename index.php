b2f0e2015ae79eb12a9cd5d8b78fd993b37b9127
yibrah <yibrahg1@gmail.com>
2016-09-12 08:13:02 -0700

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
   <a href="http://htmlpreview.github.io/?https://github.com/yibrah/MIT-Projects/master/about.php">About</a> | 
   <a href="http://htmlpreview.github.io/?https://github.com/yibrah/MIT-Projects/master/services.php">Services</a> | 
   <a href="http://htmlpreview.github.io/?https://github.com/yibrah/MIT-Projects/master/contact.php">Contact</a>
-  <br/>
+  <br/><br/><br/>
+  Enter a value : <br/>
   <form class="form-horizontal" role="form" name='loginform' method="POST" action="http://htmlpreview.github.io/?https://github.com/yibrah/MIT-Projects/master/about.php" >
     <input type="text" name="value" />
     <input type="submit" name="login" value="Login"/>
