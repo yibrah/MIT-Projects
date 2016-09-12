27e6262ca5456301e7b281e302155a999ff41dfc
yibrah <yibrahg1@gmail.com>
2016-09-12 08:04:58 -0700

    Update services.php

--- services.php
+++ services.php
@@ -1,6 +1,6 @@
 <html>
 <body>
   <h2> These are our services</h2>
-  <a href="http://htmlpreview.github.io/?https://github.com/yibrah/MIT-Projects/master/index.php">Home</a>
+  Back to <a href="http://htmlpreview.github.io/?https://github.com/yibrah/MIT-Projects/master/index.php">Home</a>
 </body>
 </html>
